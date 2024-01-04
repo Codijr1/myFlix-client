1296faa31b05b7cd41061148b9a907fe96db7489
Codi <cjrobards@gmail.com>
2024-01-04 19:26:39 -0300

    Task 3.3
    updates to satisfy task3.3 requirements

--- src/Components/MainView/main-view.jsx
+++ src/Components/MainView/main-view.jsx
@@ -1,11 +1,56 @@
+import { useState } from "react";
+import { MovieCard } from "./movie-card/movie-card";
+import { MovieView } from "./movie-view/movie-view";
+
 export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "Aliens",
+      image: "https://upload.wikimedia.org/wikipedia/en/f/fb/Aliens_poster.jpg",
+      director: "James Cameron",
+    },
+    {
+      id: 2,
+      title: "Spirited Away",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/d/db/Spirited_Away_Japanese_poster.png",
+      director: "Hayao Miyazaki",
+    },
+    {
+      id: 3,
+      title: "Whiplash",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/0/01/Whiplash_poster.jpg",
+      director: "Damien Chazelle",
+    },
+  ]);
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+  if (selectedMovie) {
     return (
-      <div>
-        <div>Eloquent JavaScript</div>
-        <div>Mastering JavaScript Functional Programming</div>
-        <div>JavaScript: The Good Parts</div>
-        <div>JavaScript: The Definitive Guide</div>
-        <div>The Road to React</div>
-      </div>
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
     );
   }
+
+  if (movies.length === 0) {
+    return <div>The list is empty</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movies) => (
+        <MovieCard
+          key={movies.id}
+          movieData={movies}
+          onMovieClick={() => {
+            setSelectedMovie(movies);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
